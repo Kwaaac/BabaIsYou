7a49b55e1afe74aa894ed40ea0de19a8c207b548
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-05-27 21:34:02 +0200

    pom/jar wip

--- src/main/java/fr/esipe/info/main/Main.java
+++ src/main/java/fr/esipe/info/main/Main.java
@@ -7,9 +7,14 @@ import fr.esipe.info.manager.LevelManager;
 import fr.umlv.zen5.Application;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(new BufferedReader(
+                new InputStreamReader(Level.class.getResourceAsStream("/levels/level00.txt"))).lines().collect(Collectors.joining("\n")));
 
         Application.run(new Color(0f, 0f, 0f, .0f), context -> {
 
