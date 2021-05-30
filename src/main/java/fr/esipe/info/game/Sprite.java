76755f22a48644ccce7b8094642e4932601af527
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-05-30 11:26:44 +0200

    Painting

--- src/main/java/fr/esipe/info/game/Sprite.java
+++ src/main/java/fr/esipe/info/game/Sprite.java
@@ -10,18 +10,31 @@ import java.util.Objects;
 
 public class Sprite {
     BufferedImage image;
+    Color colorTheme = Color.RED;
 
-    public Sprite(String filename) {
+    public Sprite(String filename, Color colorTheme) {
         Objects.requireNonNull(filename);
         try {
             this.image = ImageIO.read(Objects.requireNonNull(Sprite.class.getResourceAsStream(filename)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        this.colorTheme = colorTheme;
+    }
+
+    public void paintComponent(Graphics g) {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                if (image.getRGB(i, j) == Color.WHITE.getRGB()) {
+                    image.setRGB(i, j, colorTheme.getRGB());
+                }
+            }
+        }
     }
 
     public void draw(Graphics2D graphics, int x, int y) {
         var cellSize = GameManager.getInstance().getCellSize();
+        paintComponent(graphics);
         graphics.drawImage(image, y * cellSize, x * cellSize, cellSize, cellSize, null);
     }
 }
--- src/main/java/fr/esipe/info/game/enums/Legend.java
+++ src/main/java/fr/esipe/info/game/enums/Legend.java
@@ -1,52 +1,55 @@
 package fr.esipe.info.game.enums;
 
+import java.awt.*;
 import java.util.Objects;
 
 public enum Legend {
     /**
      * Blank
      */
-    BLANK('_', Type.ENTITY, "BLANK", ColorPrint.ANSI_RESET, -1, ""),
+    BLANK('_', Type.ENTITY, "BLANK", ColorPrint.ANSI_RESET, new Color(1f, 1f, 1f), -1, ""),
     /**
      * Nouns
      */
-    BABA_NOUN('B', Type.NOUN, "BABA", ColorPrint.ANSI_GREEN, 1, "/Sprites/BABA/noun/text_baba_0_1.png"),
-    FLAG_NOUN('F', Type.NOUN, "FLAG", ColorPrint.ANSI_YELLOW, 1, "/Sprites/FLAG/noun/text_flag_0_1.png"),
-    WALL_NOUN('W', Type.NOUN, "WALL", ColorPrint.ANSI_GREEN, 1, "/Sprites/WALL/noun/text_wall_0_1.png"),
-    ROCK_NOUN('R', Type.NOUN, "ROCK", ColorPrint.ANSI_GREEN, 1, "/Sprites/ROCK/noun/text_rock_0_1.png"),
-    SKULL_NOUN('K', Type.NOUN, "SKULL", ColorPrint.ANSI_GREEN, 1, "/Sprites/SKULL/noun/text_skull_0_1.png"),
+    BABA_NOUN('B', Type.NOUN, "BABA", ColorPrint.ANSI_GREEN, new Color(193, 5, 79), 1, "/Sprites/BABA/noun/text_baba_0_1.png"),
+    FLAG_NOUN('F', Type.NOUN, "FLAG", ColorPrint.ANSI_YELLOW, new Color(255, 240, 0), 1, "/Sprites/FLAG/noun/text_flag_0_1.png"),
+    WALL_NOUN('W', Type.NOUN, "WALL", ColorPrint.ANSI_GREEN, new Color(78, 76, 64), 1, "/Sprites/WALL/noun/text_wall_0_1.png"),
+    ROCK_NOUN('R', Type.NOUN, "ROCK", ColorPrint.ANSI_GREEN, new Color(134, 81, 41), 1, "/Sprites/ROCK/noun/text_rock_0_1.png"),
+    SKULL_NOUN('K', Type.NOUN, "SKULL", ColorPrint.ANSI_GREEN, new Color(183, 57, 24), 1, "/Sprites/SKULL/noun/text_skull_0_1.png"),
 
     /**
      * Entity
      */
-    BABA_ENTITY('b', Type.ENTITY, "BABA", ColorPrint.ANSI_RED, 1, "/Sprites/BABA/entity/baba_0_1.png"),
-    FLAG_ENTITY('f', Type.ENTITY, "FLAG", ColorPrint.ANSI_WHITE, 0, "/Sprites/FLAG/entity/flag_0_1.png"),
-    WALL_ENTITY('w', Type.ENTITY, "WALL", ColorPrint.ANSI_WHITE, 0, "/Sprites/WALL/entity/wall_0_1.png"),
-    ROCK_ENTITY('r', Type.ENTITY, "ROCK", ColorPrint.ANSI_WHITE, 0, "/Sprites/ROCK/entity/rock_0_1.png"),
-    SKULL_ENTITY('k', Type.ENTITY, "SKULL", ColorPrint.ANSI_WHITE, 0, "/Sprites/SKULL/entity/skull_24_1.png"),
+    BABA_ENTITY('b', Type.ENTITY, "BABA", ColorPrint.ANSI_RED, Color.WHITE, 1, "/Sprites/BABA/entity/baba_0_1.png"),
+    FLAG_ENTITY('f', Type.ENTITY, "FLAG", ColorPrint.ANSI_WHITE, new Color(255, 240, 0), 0, "/Sprites/FLAG/entity/flag_0_1.png"),
+    WALL_ENTITY('w', Type.ENTITY, "WALL", ColorPrint.ANSI_WHITE, new Color(42, 46, 76, 255), 0, "/Sprites/WALL/entity/wall_0_1.png"),
+    ROCK_ENTITY('r', Type.ENTITY, "ROCK", ColorPrint.ANSI_WHITE, new Color(177, 150, 15), 0, "/Sprites/ROCK/entity/rock_0_1.png"),
+    SKULL_ENTITY('k', Type.ENTITY, "SKULL", ColorPrint.ANSI_WHITE, new Color(183, 57, 24), 0, "/Sprites/SKULL/entity/skull_24_1.png"),
 
     /**
      * Operator
      */
-    IS('s', Type.OPERATOR, "IS", ColorPrint.ANSI_CYAN_BACKGROUND, 1, "/Sprites/OPERATOR/IS/text_is_0_1.png"),
+    IS('s', Type.OPERATOR, "IS", ColorPrint.ANSI_CYAN_BACKGROUND, Color.WHITE, 1, "/Sprites/OPERATOR/IS/text_is_0_1.png"),
 
     /**
      * Property
      */
-    YOU('U', Type.PROPERTY, "YOU", ColorPrint.ANSI_YELLOW_BACKGROUND, 1, "/Sprites/PROPERTY/YOU/text_you_0_1.png"),
-    STOP('S', Type.PROPERTY, "STOP", ColorPrint.ANSI_YELLOW_BACKGROUND, 1, "/Sprites/PROPERTY/STOP/text_stop_0_1.png"),
-    PUSH('P', Type.PROPERTY, "PUSH", ColorPrint.ANSI_YELLOW_BACKGROUND, 1, "/Sprites/PROPERTY/PUSH/text_push_0_1.png"),
-    WIN('!', Type.PROPERTY, "WIN", ColorPrint.ANSI_YELLOW_BACKGROUND, 1, "/Sprites/PROPERTY/WIN/text_win_0_1.png"),
-    DEFEAT('X', Type.PROPERTY, "DEFEAT", ColorPrint.ANSI_YELLOW_BACKGROUND, 1, "/Sprites/PROPERTY/DEFEAT/text_defeat_0_1.png");
+    YOU('U', Type.PROPERTY, "YOU", ColorPrint.ANSI_YELLOW_BACKGROUND, new Color(193, 5, 79), 1, "/Sprites/PROPERTY/YOU/text_you_0_1.png"),
+    STOP('S', Type.PROPERTY, "STOP", ColorPrint.ANSI_YELLOW_BACKGROUND, new Color(20, 90, 50), 1, "/Sprites/PROPERTY/STOP/text_stop_0_1.png"),
+    PUSH('P', Type.PROPERTY, "PUSH", ColorPrint.ANSI_YELLOW_BACKGROUND, new Color(134, 81, 41), 1, "/Sprites/PROPERTY/PUSH/text_push_0_1.png"),
+    WIN('!', Type.PROPERTY, "WIN", ColorPrint.ANSI_YELLOW_BACKGROUND, new Color(255, 240, 0), 1, "/Sprites/PROPERTY/WIN/text_win_0_1.png"),
+    DEFEAT('X', Type.PROPERTY, "DEFEAT", ColorPrint.ANSI_YELLOW_BACKGROUND, new Color(183, 57, 24), 1, "/Sprites/PROPERTY/DEFEAT/text_defeat_0_1.png");
 
     private final char aChar;
     private final Type type;
     private final String name;
     private final ColorPrint textColor;
+    private final Color graphicColor;
     private final int weight;
     private final String imagePath;
 
-    Legend(char aChar, Type type, String name, ColorPrint textColor, int weight, String imagePath) {
+    Legend(char aChar, Type type, String name, ColorPrint textColor, Color graphicColor, int weight, String imagePath) {
+        Objects.requireNonNull(graphicColor);
         Objects.requireNonNull(type);
         Objects.requireNonNull(name);
         Objects.requireNonNull(textColor);
@@ -57,6 +60,7 @@ public enum Legend {
         this.textColor = textColor;
         this.weight = weight;
         this.imagePath = imagePath;
+        this.graphicColor = graphicColor;
     }
 
     public char getaChar() {
@@ -85,5 +89,9 @@ public enum Legend {
 
     public String getImageStream() {
         return imagePath;
+    }
+
+    public Color getGraphicColor() {
+        return graphicColor;
     }
 }
