d738cfa932851bc74c649ec03793b6e5d3ad1189
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-05-30 21:02:58 +0200

    Music Cut with M

--- src/main/java/fr/esipe/info/main/Main.java
+++ src/main/java/fr/esipe/info/main/Main.java
@@ -6,24 +6,14 @@ import fr.esipe.info.manager.GameManager;
 import fr.esipe.info.manager.LevelManager;
 import fr.umlv.zen5.Application;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
 import java.awt.*;
-import java.io.IOException;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
 
         AudioInputStream audioInputStream;
-        try {
-            System.out.println(Main.class.getResourceAsStream("/Music/baba.wav"));
-            audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(Main.class.getResourceAsStream("/Music/baba.wav")));
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-            e.printStackTrace();
-        }
+
 
         Application.run(new Color(0f, 0f, 0f, 1f), context -> {
             GameManager gameManager = GameManager.getInstance();
@@ -31,7 +21,7 @@ public class Main {
             gameManager.setWidth((int) context.getScreenInfo().getWidth());
 
             EncryptionDecorator encoded = new EncryptionDecorator(Level.LEVEL_00.getFileStream());
-            LevelManager levelManager = new LevelManager("bob", encoded);
+            LevelManager levelManager = new LevelManager("bob", encoded, "/Music/baba.wav");
             gameManager.setLevelManager(levelManager);
             context.renderFrame(levelManager::render);
 
