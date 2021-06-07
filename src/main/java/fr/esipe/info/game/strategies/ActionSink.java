22daf227ca412e49c131da3ff8ef265c9ca49cc3
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-06-07 21:28:54 +0200

    Rule Set update

--- src/main/java/fr/esipe/info/game/strategies/ActionSink.java
+++ src/main/java/fr/esipe/info/game/strategies/ActionSink.java
@@ -7,7 +7,7 @@ import fr.esipe.info.manager.GameManager;
 public class ActionSink implements ActionStrategy {
     @Override
     public void execute(Rules rules, BoardEntity from, BoardEntity to) {
-        if(to != null && !from.equals(to)){
+        if (to != null && !from.equals(to)) {
             System.out.println("Application de SINK");
             GameManager.getInstance().removeGameObject(to);
             GameManager.getInstance().removeGameObject(from);
